dac658817ac0e2725d395f5cf51a3b9d5752ffc4
Nemes Máté <nemes.mate03@gmail.com>
2024-10-16 00:45:04 +0200

    mag commented

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -38,6 +38,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
+    //random helyett mag
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
